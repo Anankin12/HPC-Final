7d7545b69f2efd89d1bd4d9fca4b0d85d7944949
Anankin12 <alberto.russo@studenti.unitn.it>
2024-05-15 17:27:14 +0000

    Data

--- Ex2/Pintus.sh
+++ Ex2/Pintus.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH -A dssc
-#SBATCH -p THIN
+#SBATCH -p EPYC
 #SBATCH --job-name=Raceya
-#SBATCH --nodes=1
+#SBATCH --nodes=3
 #SBATCH --exclusive # The CPU is an Intel Xeon Gold 6126, with 12 cores per socket
 #SBATCH --time=00:59:00
 #SBATCH --exclude=fat[001-002]	# Exclude the fat nodes since they are WRONGLY placed in the THIN partition, but have
@@ -22,8 +22,8 @@ xl="-2"
 yl="-2"
 xr="2"
 yr="2"
-width="2048"
-height="2048"
+width="32738"
+height="32738"
 max_iterations="65535"
 
 # Dynamically determine the number of logical processors (threads)
@@ -37,7 +37,7 @@ echo "Running with NO -NP threads..."
 
 # Adjust MPI command to handle oversubscription gracefully
 # export OMP_NUM_THREADS=1
-command="mpiexec $program_name $xl $yl $xr $yr $width $height $max_iterations"
+command="mpiexec -np 384  $program_name $xl $yl $xr $yr $width $height $max_iterations"
 
 # Execute the MPI program and measure the time
 TIMEFORMAT=%R
