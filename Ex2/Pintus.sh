83faccea87f8aff9793849409b6a5744368588f8
Anankin12 <alberto.russo@studenti.unitn.it>
2024-05-20 11:17:53 +0200

    Added summary graph

--- Ex2/Pintus.sh
+++ Ex2/Pintus.sh
@@ -3,9 +3,9 @@
 #SBATCH -A dssc
 #SBATCH -p EPYC
 #SBATCH --job-name=Raceya
-#SBATCH --nodes=3
+#SBATCH --nodes=2
 #SBATCH --exclusive # The CPU is an Intel Xeon Gold 6126, with 12 cores per socket
-#SBATCH --time=00:59:00
+#SBATCH --time=02:00:00
 #SBATCH --exclude=fat[001-002]	# Exclude the fat nodes since they are WRONGLY placed in the THIN partition, but have
                 # a different CPU model (Intel Xeon Gold 6154) and 18 cores per socket, which would
 				# make the data incomparable and the script would be wrong
@@ -22,8 +22,8 @@ xl="-2"
 yl="-2"
 xr="2"
 yr="2"
-width="512"
-height="1024"
+width="4096"
+height="4096"
 max_iterations="65535"
 
 # Dynamically determine the number of logical processors (threads)
